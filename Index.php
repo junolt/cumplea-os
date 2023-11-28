1973060d611972de5875ac7feacf4630c8d61f71
junolt <152246556+junolt@users.noreply.github.com>
2023-11-27 23:46:30 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title> CUMPLEAÑOS  </title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/forma.css">
+    <body>
+        <form action=" " method="POST" >
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+                <CEnter><h1>FELIS CUMPLEAÑOS</h1> </CEnter>
+                    <center><h1>ESTEFA</h1> </center>
+        <section class="portfolio-experiment"> 
+            <a href="fotos.php">
+                <span class="text">FELIZ CUMPLE</span>
+                <span class="line -right"></span>
+                <span class="line -top"></span>
+                <span class="line -left"></span>
+                <span class="line -bottom"></span>
+            </a>
+        </section>
+        </form>
+    </body>
+</html>
